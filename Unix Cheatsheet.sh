2b6b7c732ccfc3f1971d7443ff667188f7cdb1e1
Kishan Karunaratne <kishan@karu.io>
2014-08-13 10:34:02 -0700

    added cmds

---
+++ Unix Cheatsheet.sh
@@ -0,0 +1,81 @@
+#Basic
+=======
+cd
+ls
+pwd
+mv
+cp
+rm
+mkdir
+clear
+exit
+
+#Utility
+========
+ssh
+pssh
+scp
+rsync
+kill
+service
+tar -xzvf
+script
+awk
+find . -name '*.cfg'
+screen
+uname -a
+whatis
+
+#Filesystem
+==========
+df -f
+mount
+umount
+chmod
+chown
+which
+whoami
+hostname
+free -m
+head -n 4 /proc/meminfo 
+
+#Package Manager
+===============
+apt-get update
+apt-get upgrade
+apt-get install
+apt-get remove
+
+#Text
+=========
+echo
+cat
+head
+tail
+more
+less
+grep -rni
+diff
+wc -l
+
+#Grep
+=======
+grep "pattern" fileName
+-x (exact match)
+-v (exact unmatch)
+-w (full words)
+-o (output matched string, not line)
+-i (ignore case)
+-r (curr dir, and subdirs)
+-c (count # matches)
+
+#Network
+=============
+ifconfig
+ping
+curl
+http
+ncat
+nmap
+netstat
+
